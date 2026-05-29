9b0d2feb5d23bec6dedab839dc087af5039cefc7
KoteswaraPrasad <koteswaraprasad274@gmail.com>
2026-05-29 12:21:48 +0530

    a

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'red !important' }}>
+    <AppBar position="static" style={{ backgroundColor: 'red' }} sx={{ backgroundColor: 'red' }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Button color="inherit" component={Link} to="/">Home</Button>
